dad3a830fae615f2c73e4afe2867c5feb765af5f
saeheeshin <saeheeshin@gmail.com>
2023-03-04 11:35:14 -0500

    fixed gtm tag; added id option for button component

--- src/components/global/button/button.component.jsx
+++ src/components/global/button/button.component.jsx
@@ -1,9 +1,13 @@
 import "./button.styles.scss";
 import { Link } from "react-router-dom";
 
-const Button = ({ details: { buttonType, link, icon }, children }) => {
+const Button = ({ details: { id, buttonType, link, icon }, children }) => {
   return (
-    <Link className={`btn btn__${buttonType}`} to={link}>
+    <Link
+      {...(id === undefined ? "" : { id: id })}
+      className={`btn btn__${buttonType}`}
+      to={link}
+    >
       <strong>
         {icon && <span className="material-symbols-rounded">{icon}</span>}
         {children}
@@ -13,13 +17,14 @@ const Button = ({ details: { buttonType, link, icon }, children }) => {
 };
 
 const Anchor = ({
-  details: { buttonType, link, icon, rel, target },
+  details: { id, buttonType, link, icon, rel, target },
   children,
 }) => {
   return (
     <a
       className={`btn btn__${buttonType}`}
       href={link}
+      {...(id === undefined ? "" : { id: id })}
       {...(target === undefined ? "" : { target: target })}
       {...(rel === undefined ? "" : { rel: rel })}
     >
