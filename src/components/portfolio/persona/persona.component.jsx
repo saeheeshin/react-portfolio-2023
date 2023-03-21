b43862e1a8fc0a2cc1200a6624f58300f234745b
saeheeshin <saeheeshin@gmail.com>
2023-03-21 07:26:56 -0400

    persona html

--- src/components/portfolio/persona/persona.component.jsx
+++ src/components/portfolio/persona/persona.component.jsx
@@ -1,7 +1,54 @@
 import "./persona.styles.scss";
 
 const Persona = () => {
-  return <div></div>;
+  return (
+    <article className="persona">
+      <h2 className="persona__name">John Smith</h2>
+
+      <table className="persona__details">
+        <tbody>
+          <tr>
+            <th>Age</th>
+            <td>29</td>
+          </tr>
+          <tr>
+            <th>Location</th>
+            <td>Toronto</td>
+          </tr>
+          <tr>
+            <th>Occupation</th>
+            <td>Student</td>
+          </tr>
+          <tr>
+            <th>Family</th>
+            <td>None</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <q>Why isn't it working?!</q>
+
+      <div className="persona__pain-goal">
+        <div className="persona__pain">
+          <h3>Pain</h3>
+          <ul>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+          </ul>
+        </div>
+
+        <div className="persona__goal">
+          <h2>Goal</h2>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+          </ul>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default Persona;
